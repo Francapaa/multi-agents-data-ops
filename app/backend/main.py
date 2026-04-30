697195a79effb4a43c3f1e07fbae45a538554ede
Francapaa <franciscocaparruva@gmail.com>
2026-04-30 20:14:40 -0300

    Initial commit

---
+++ app/backend/main.py
@@ -0,0 +1,21 @@
+import os
+from fastapi import FastAPI
+from pydantic import BaseModel
+from langgraph.graph import StateGraph, END
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    value: int 
+
+
+@app.get("/")
+async def read_root():
+    return {"HELLO": "WORLD"}
+
+@app.put("/item")
+async def create_item(item: Item):
+    
+    return {'item': item, "name": item.name, "value": item.value}
